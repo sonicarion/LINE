5ab41424ba74424ce27b0616dbbd525a6332cf2e
sonicarion <b5375486@gmail.com>
2017-07-13 10:57:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Sonic test'
+				'text' => $_SERVER['REMOTE_ADDR']
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
